5177129a2d4455ddc7934079b45c7fd34858d7be
doogie97 <chris_97@naver.com>
2022-11-26 20:40:20 +0900

    💎feat : FruitStore 생성

---
+++ JuiceMaker_SwiftUI/Model/FruitStore.swift
@@ -0,0 +1,12 @@
+//
+//  FruitStore.swift
+//  JuiceMaker_SwiftUI
+//
+//  Created by 최최성균 on 2022/11/26.
+//
+
+import Foundation
+
+final class FruitStore: ObservableObject {
+    @Published var Fruits = []
+}
