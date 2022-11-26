039c4ac2301251c0f98eca9d7eddda1a79ef0789
doogie97 <chris_97@naver.com>
2022-11-26 20:29:33 +0900

    💎feat : Juice Maker SwiftUI버전 프로젝트 생성

---
+++ JuiceMaker_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JuiceMaker_SwiftUI
+//
+//  Created by 최최성균 on 2022/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JuiceMaker_SwiftUI/JuiceMaker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMaker_SwiftUIApp.swift
+//  JuiceMaker_SwiftUI
+//
+//  Created by 최최성균 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct JuiceMaker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
