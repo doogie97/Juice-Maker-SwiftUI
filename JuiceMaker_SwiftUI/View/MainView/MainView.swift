31f4cc949a43e8419ba283d40b040b30deb549c6
doogie97 <chris_97@naver.com>
2022-11-28 09:58:19 +0900

    💎feat : MainView 생성

---
+++ JuiceMaker_SwiftUI/View/MainView/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  JuiceMaker_SwiftUI
+//
+//  Created by 최최성균 on 2022/11/28.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
