ec8d612b47a69368e176f81da34f414b3f01337c
doogie97 <chris_97@naver.com>
2022-11-28 15:20:22 +0900

    ✏️chore : dummydata 추가

---
+++ JuiceMaker_SwiftUI/Model/FruitsArrayForApp.swift
@@ -0,0 +1,14 @@
+//
+//  FruitsArrayForApp.swift
+//  JuiceMaker_SwiftUI
+//
+//  Created by 최최성균 on 2022/11/26.
+//
+
+let FruitsArrayForApp: [FruitName : Fruit] = [
+    .strawberry : Fruit(id: 0, fruitImage: "🍓", count: 10),
+    .banana : Fruit(id: 1, fruitImage: "🍌", count: 10),
+    .pineapple : Fruit(id: 2, fruitImage: "🍍", count: 10),
+    .kiwi : Fruit(id: 3, fruitImage: "🥝", count: 10),
+    .mango : Fruit(id: 4, fruitImage: "🥭", count: 10),
+]
